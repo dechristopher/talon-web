d23d1d2eed4ed6600559af647078a0cb935c9e34
Andrew DeChristopher <dechristophera@wit.edu>
2016-12-19 21:29:21 -0500

    Created random string generator

---
+++ helpers/randomstring.js
@@ -0,0 +1,10 @@
+module.exports = function (length) {
+    var text = '';
+    var possible = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
